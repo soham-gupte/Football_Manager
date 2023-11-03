1ff41a8f104707848373f3b6ff337736a696fac6
Vadi26 <vadirajj163@gmail.com>
2023-11-04 00:21:00 +0530

    Completed till 52:33

---
+++ login-signup/client/src/App.js
@@ -0,0 +1,52 @@
+import './App.css';
+import { useState } from "react";
+import Axios from 'axios'
+
+function App() {
+
+  // Create a state for each variable
+  const [name, setName] = useState("");
+  const [age, setAge] = useState(0);
+  const [country, setCountry] = useState("");
+  const [position, setPosition] = useState("");
+  const [wage, setWage] = useState(0);
+
+  const addEmployee = () => {
+    Axios.post('http://localhost:3001/create', {
+      name: name, 
+      age: age, 
+      country: country, 
+      position: position, 
+      wage: wage,}).then(()=> {
+        console.log("SUCCESS");
+      })
+  }
+
+  return (
+    <div className="App">
+      <label>Name : </label>
+      <input type="text" onChange={(event) => {
+        setName(event.target.value);
+      }}/>
+      <label>Age : </label>
+      <input type="number" onChange={(event) => {
+        setAge(event.target.value);
+      }}/>
+      <label>Country : </label>
+      <input type="text" onChange={(event) => {
+        setCountry(event.target.value);
+      }}/>
+      <label>Position : </label>
+      <input type="text" onChange={(event) => {
+        setPosition(event.target.value);
+      }}/>
+      <label>Wage : </label>
+      <input type="number" onChange={(event) => {
+        setWage(event.target.value);
+      }}/>
+      <button onClick={addEmployee}>Add Employee</button>
+    </div>
+  );
+}
+
+export default App;
