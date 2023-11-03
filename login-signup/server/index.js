f555f25f4944752b50f61cb4fdf7dd6a90aed04f
Vadi26 <vadirajj163@gmail.com>
2023-11-04 00:21:00 +0530

    Completed till 52:33

---
+++ login-signup/server/index.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const app = express();
+const mysql = require('mysql');
+const cors = require('cors');
+
+app.use(cors());
+app.use(express.json());
+
+const db = mysql.createConnection({
+    user: 'root', 
+    host: 'localhost',
+    password: 'Vadi@2606',
+    database: 'employeeSystem'
+});
+
+app.post('/create', (req, res) => {
+    const name = req.body.name;
+    const age = req.body.age;
+    const country = req.body.country;
+    const position = req.body.position;
+    const wage = req.body.wage;
+
+    db.query('INSERT INTO employees (name, age, country, position, wage) VALUES(?,?,?,?,?)', 
+    [name, age, country, position, wage], 
+    (err, result) => {
+        if (err) {
+            console.log(err)
+        } else {
+            res.send("Values Inserted");
+        }
+    })
+})
+
+app.listen(3001, () => {
+    console.log("SERVER IS RUNNING ON PORT 3001")
+})
