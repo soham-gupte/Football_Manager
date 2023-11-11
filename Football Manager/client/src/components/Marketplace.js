4fd904290f2953ba8fb5a6f908dc37213675bfae
Vadi26 <vadirajj163@gmail.com>
2023-11-11 17:03:40 +0530

    Marketplace without UI done

--- Football Manager/client/src/components/Marketplace.js
+++ Football Manager/client/src/components/Marketplace.js
@@ -1,10 +1,53 @@
-// import { useState } from "react";4
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom"
+import Axios from "axios";
+import { useState, useEffect } from "react";
 
 export function Marketplace() {
+    const [team_name, setTeamName] = useState('');
+    const [players, setPlayers] = useState([]);
+
+    useEffect(() => {
+        // Retrieve username from localStorage
+        const storedTeamName = localStorage.getItem('team_name');
+
+        // Update state with the retrieved username
+        if (storedTeamName) {
+            setTeamName(storedTeamName);
+        }
+
+        // Run the retrievemarketplace function when the component mounts
+        Axios.post('http://localhost:3001/retreivemarketplace', {
+            team_name: team_name,
+        }).then((response) => {
+            console.log("Marketplace retrieved successfully !");
+            // Ensure response.data.player_name is defined before setting it in the state
+            if (response.data.player_name) {
+                // Assuming all arrays have the same length and are synchronized
+                const newPlayers = response.data.player_name.map((name, index) => ({
+                    player_name: name,
+                    position: response.data.position[index],
+                    nationality: response.data.nationality[index],
+                    value: response.data.value[index]
+                }));
+                setPlayers(newPlayers);
+            }
+        }).catch((error) => {
+            console.log("Some error occurred :(");
+        });
+
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [team_name]);
+
     return (
-        <div className="signup-login-btn">
+        <div className="marketplace">
+            <h1>{team_name}</h1>
             <h1>MARKETPLACE</h1>
+            <ul>
+                {players.map((player, index) => (
+                    <li key={index}>
+                        <strong>{player.player_name}</strong> - {player.position}, {player.nationality}, Value: {player.value}
+                    </li>
+                ))}
+            </ul>
         </div>
-    )
+    );
 }
--- Football Manager/client/src/components/Menu.js
+++ Football Manager/client/src/components/Menu.js
@@ -1,3 +1,4 @@
+import { Axios } from "axios";
 import { useState, useEffect } from "react";
 
 export function Menu() {
